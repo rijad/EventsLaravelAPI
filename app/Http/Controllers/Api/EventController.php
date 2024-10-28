b3140fc8117782b6950e0deec92c220dce0d2f61
rijad <i.rijad@gmail.com>
2024-10-28 22:07:30 +0100

    introducing resources

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return  EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -32,7 +33,7 @@ class EventController extends Controller
 
         $event = Event::create($data);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -40,10 +41,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        if ($event)
-            return $event;
-        else
-            return response(status: 204);   
+        $event->load('user','attendees'); // loads user data    
+        return new EventResource($event);
     }
 
     /**
@@ -60,7 +59,7 @@ class EventController extends Controller
 
         $event->update($data);
 
-        return $event;
+        return new EventResource($event) ;
     }
 
     /**
