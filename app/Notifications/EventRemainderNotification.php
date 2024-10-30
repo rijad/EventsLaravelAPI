e8cc4a8e86e4b4a7f12aea4d4640a2933baa3c94
rijad <i.rijad@gmail.com>
2024-10-30 14:12:07 +0100

    Queue implemented

--- app/Notifications/EventRemainderNotification.php
+++ app/Notifications/EventRemainderNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EventRemainderNotification extends Notification
+class EventRemainderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
